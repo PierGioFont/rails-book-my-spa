4bd2b67920dc786f10cc74d5da0c69437a628def
Adrien Poly <adrienpoly@gmail.com>
2017-02-20 17:43:25 +0100

    initial gem install of attachinary"
    "

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :spas, only: [:index, :show, :new, :create]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Attachinary::Engine => "/attachinary"
 end
