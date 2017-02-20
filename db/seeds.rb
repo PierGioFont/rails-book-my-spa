150d6a6f9704fd0cf8d465dd4437d559952d30e7
Adrien Poly <adrienpoly@gmail.com>
2017-02-20 18:22:13 +0100

    fix bug in db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Massage.destroy_all
 Spa.destroy_all
 User.destroy_all
 
 user = User.new(email: 'adrienpoly@gmail.com', password: '123456')
 user.save
 
-Spa.create(name: 'caudalie', user_id: user.id, description: 'super spa dans les vignes', country: 'france')
+spa = Spa.create(name: 'caudalie', user_id: user.id, description: 'super spa dans les vignes', country: 'france')
 
 Massage.create(name: 'Instant lift', spa_id: spa.id, description: 'Soin du visage', price: 60)
 Massage.create(name: 'Soin essentiel minceur', spa_id: spa.id, description: 'Gommages et modelages', price: 65)
