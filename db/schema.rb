36c68a6b3bf8371cf7d07ba5713b4fc24ede844d
Madeleine Triboy <triboy.m@gmail.com>
2017-02-21 16:43:29 +0100

    add rating to bookings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221120020) do
+ActiveRecord::Schema.define(version: 20170221154202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170221120020) do
     t.integer  "massage_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "rating"
     t.index ["massage_id"], name: "index_bookings_on_massage_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
