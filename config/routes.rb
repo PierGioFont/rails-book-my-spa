00de9640f2af2e3be64bdc8c36a3d4998d6a3d21
Adrien Poly <adrienpoly@gmail.com>
2017-02-21 17:45:21 +0100

    bokkings list ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :spas, only: [:index, :show, :new, :create]  do
     resources :bookings, only: [:new, :create]
   end
+
+  resources :bookings, only: [:index, :destroy]
+
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
