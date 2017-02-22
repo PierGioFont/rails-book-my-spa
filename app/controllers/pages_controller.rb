de7a0b79f975be49a0c5b5584047e166e545b92a
Madeleine Triboy <triboy.m@gmail.com>
2017-02-22 17:16:49 +0100

    le titre est plus accrocheur et les cards aléatoires

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @spas = Spa.take(3)
+    @spas = Spa.limit(3).order("RANDOM()")
   end
 end
