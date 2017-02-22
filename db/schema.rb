46560bc2dab6de8b71b51c984ea3abc5f9c273d6
Lucie La Sagna <lucie.lasagna@essec.edu>
2017-02-22 13:12:52 +0100

    add a my spas' page for owner

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221174033) do
+ActiveRecord::Schema.define(version: 20170222101511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
