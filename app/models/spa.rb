fb52696a39fea857d6443f51a65d8c8357022db9
Adrien Poly <adrienpoly@gmail.com>
2017-02-22 11:41:32 +0100

    added geoloc search with nearby

--- app/models/spa.rb
+++ app/models/spa.rb
@@ -3,6 +3,8 @@ class Spa < ApplicationRecord
   validates :name, presence: true
   has_many :massages
   has_attachment :photo
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 
   def equipments
     self.amenities['equipments'].split(';')
